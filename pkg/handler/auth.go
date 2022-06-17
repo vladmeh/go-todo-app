e7e8ccea3b3c3fc0bcf0cdab236370606d090855
vladmeh <vladmeh@gmail.com>
2022-06-18 01:07:04 +0300

    #7. Authentication & JWT token.

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -24,6 +24,25 @@ func (h *Handler) singUp(c *gin.Context) {
 	})
 }
 
-func (h *Handler) singIn(c *gin.Context) {
+type singInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
+func (h *Handler) singIn(c *gin.Context) {
+	var input singInInput
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 }
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,11 +3,22 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/vladmeh/go-todo-app"
 	"github.com/vladmeh/go-todo-app/pkg/repository"
+	"time"
 )
 
-const salt = "skjhfdkjdgh34353kwjkflkejbvb"
+const (
+	salt       = "skjhfdkjdgh34353kwjkflkejbvb"
+	tokenTTL   = 12 * time.Hour
+	signingKey = "jshdfksjd8769823kjwbjrebvjb4981"
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -20,6 +31,23 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
